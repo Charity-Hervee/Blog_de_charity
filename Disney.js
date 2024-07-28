ace47c56cd991b389b58a416f9d85155c471f1aa
Charity-Hervee <Holognoncharite@gmail.com>
2024-07-28 08:13:42 +0000

    Add files via upload

---
+++ Disney.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    if (checkbox.checked) {
+        document.body.style.backgroundColor = "blue";
+    } else {
+        document.body.style.backgroundColor = "";
+    }
+}
