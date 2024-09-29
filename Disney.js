66b00aba125286e27f2d8d6d4e275d9a9a175b16
Charity-Hervee <Holognoncharite@gmail.com>
2024-09-29 16:32:31 +0000

    Delete Disney.js

--- Disney.js
+++
@@ -1,7 +0,0 @@
-function changecolor(){
-    if (checkbox.checked) {
-        document.body.style.backgroundColor = "blue";
-    } else {
-        document.body.style.backgroundColor = "";
-    }
-}
